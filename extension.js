f39e4f9eed8900aaa347b8a7878b1907d071936e
Frank Mazzarella <mazzarella.frank@gmail.com>
2020-01-16 11:53:31 -0500

    adding status icon and a few popup menu items

--- extension.js
+++ extension.js
@@ -1,34 +1,46 @@
-/* extension.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
+const Lang = imports.lang;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const St = imports.gi.St;
 
-/* exported init */
+let panelEntry;
 
 class Extension {
-    constructor() {
-    }
+  enable() {
+    panelEntry = new GnomeNotesEntry();
+    Main.panel.addToStatusArea('GnomeNotesEntry', panelEntry);
+  }
 
-    enable() {
-    }
-
-    disable() {
-    }
+  disable() {
+    panelEntry.destroy();
+  }
 }
 
 function init() {
-    return new Extension();
+  return new Extension();
 }
+
+const GnomeNotesEntry = new Lang.Class({
+  Extends: PanelMenu.Button,
+  Name: 'GnomeNotesEntry',
+
+  _init: function () {
+    this.parent(0, 'GnomeNotesEntry');
+
+    const icon = new St.Icon({
+      icon_name: 'accessories-text-editor-symbolic',
+      style_class: 'system-status-icon'
+    });
+    this.actor.add_child(icon);
+
+    const testItem = new PopupMenu.PopupMenuItem('Test Item');
+    const settingsMenuItem = new PopupMenu.PopupMenuItem('Settings');
+
+    this.menu.addMenuItem(testItem);
+    this.menu.addMenuItem(settingsMenuItem);
+
+    testItem.connect('activate', Lang.bind(this, () => log('TEST CLICKED')));
+    settingsMenuItem.connect('activate', Lang.bind(this, () => log('SETTINGS CLICKED')));
+  }
+});
