2009682388d2dc8b62ddc692b39ef8809c7b3e6d
Frank Mazzarella <mazzarella.frank@gmail.com>
2020-01-16 20:59:14 -0500

    refactoring gnome notes entry into its own file like a civilized developer should do

--- extension.js
+++ extension.js
@@ -1,15 +1,10 @@
-const Lang = imports.lang;
 const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
-const St = imports.gi.St;
 const CurrentExtension = imports.misc.extensionUtils.getCurrentExtension();
 
-const Storage = CurrentExtension.imports.Storage;
+const GnomeNotesEntry = CurrentExtension.imports.GnomeNotesEntry.handle;
 
 // TODO: if too many notes then popup will expand below bottom of screen, implement scroll
 let panelEntry;
-let notes;
 
 class Extension {
   enable() {
@@ -23,28 +18,5 @@ class Extension {
 }
 
 function init() {
-  notes = Storage.getNotes();
   return new Extension();
 }
-
-const GnomeNotesEntry = new Lang.Class({
-  Extends: PanelMenu.Button,
-  Name: 'GnomeNotesEntry',
-
-  _init: function () {
-    this.parent(0, 'GnomeNotesEntry');
-
-    const icon = new St.Icon({
-      icon_name: 'accessories-text-editor-symbolic',
-      style_class: 'system-status-icon'
-    });
-    this.actor.add_child(icon);
-
-    notes.forEach(note => {
-      const popupMenuItem = new PopupMenu.PopupMenuItem(note);
-      popupMenuItem.connect('activate', Lang.bind(this, () => log('ITEM CLICKED')));
-      this.menu.addMenuItem(popupMenuItem);
-    });
-    // this.menu.removeAll() USE TO CLEAR POPUP MENU ITEMS
-  }
-});
