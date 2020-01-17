2b40ddc29109c7e1fc8a85f9ef6fdb2ec62f64ed
Frank Mazzarella <mazzarella.frank@gmail.com>
2020-01-17 11:53:32 -0500

    adding delete button however not hooked up to any actual delete functionality

--- NoteItem.js
+++ NoteItem.js
@@ -8,9 +8,20 @@ var NoteItem = GObject.registerClass(
     _init(label) {
       super._init();
       this.label = new St.Label({ text: label, x_expand: true });
-      this.label_actor = this.label;
       this.add_child(this.label);
       this.connect('activate', Lang.bind(this, () => log('test')));
+      let hbox = new St.BoxLayout();
+      let deleteIcon = new St.Icon({
+        icon_name: 'window-close-symbolic',
+        style_class: 'popup-menu-icon popup-menu-icon-extensions-settings '
+      });
+      let deleteButton = new St.Button({ child: deleteIcon });
+      deleteButton.connect('clicked', Lang.bind(this, function () {
+        log('delete clicked');
+        this._getTopMenu().close()
+      }));
+      hbox.add_child(deleteButton);
+      this.add_child(hbox);
     }
   }
 );
